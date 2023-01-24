a42eb44d3a8ab86fc9dabf82833d901d40a75078
Max <43474218+Moxemka@users.noreply.github.com>
2023-01-25 00:44:33 +0300

    Test

---
+++ test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Figures;
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //demo circle area
+            Circle circ = new Circle(2);
+            Console.WriteLine(circ.ToString());
+
+            //demo triangle area and default type 
+            Triangle tri = new Triangle(12, 12, 2);
+            Console.WriteLine(tri.ToString());
+
+            //demo triangle area and right type 
+            Triangle rtri = new Triangle(3, 4, 5);
+            Console.WriteLine(rtri.ToString());
+
+            //demo unknown circle
+            FigureCalc fig1 = new FigureCalc(20);
+            Console.WriteLine(fig1.ToString());
+
+            //demo unknown triangle
+            FigureCalc fig2 = new FigureCalc(3, 4, 5);
+            Console.WriteLine(fig2.ToString());
+
+        }
+    }
+}
